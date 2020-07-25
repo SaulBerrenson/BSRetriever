dfed7e157d3d8370c6453fd4f8fbe21a656f693c
Saul <Saul@google.com>
2020-07-25 19:50:16 +0300

    OSM Link

---
+++ Links/OpenStreetMapLink.cs
@@ -0,0 +1,14 @@
+﻿using BSRetriever.Extantions;
+using BSRetriever.Interfaces;
+
+namespace BSRetriever.Links
+{
+    public class OpenStreetMapLink : ILinkLocation
+    {
+        
+        public string GetLink((double lat, double lon) location)
+        {
+            return $"https://www.openstreetmap.org/directions?from={location.lat.DoubleWithDot()}%2C{location.lon.DoubleWithDot()}";
+        }
+    }
+}
