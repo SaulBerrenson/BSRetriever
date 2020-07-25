ad51dade4abda2fd3c652553595889344314bbbd
Saul <Saul@google.com>
2020-07-25 17:29:13 +0300

    Created bases classes

--- Interfaces/BaseRetriver.cs
+++ Interfaces/BaseRetriver.cs
@@ -2,15 +2,42 @@
 {
     public abstract class BaseRetriver
     {
+        #region Constructors
+        protected BaseRetriver((string MCC, string MNC, string LAC, string CID) data)
+        {
+            Data = data;
+        }
+
+        protected BaseRetriver(IRetriverLocation retriverLocation, (string MCC, string MNC, string LAC, string CID) data)
+        {
+            _retriverLocation = retriverLocation;
+            Data = data;
+        }
+
+        protected BaseRetriver(IAddressRetriver retriverAddress, IRetriverLocation retriverLocation, (string MCC, string MNC, string LAC, string CID) data)
+        {
+            _retriverAddress = retriverAddress;
+            _retriverLocation = retriverLocation;
+            Data = data;
+        }
+
+        protected BaseRetriver(IAddressRetriver retriverAddress, (string MCC, string MNC, string LAC, string CID) data)
+        {
+            _retriverAddress = retriverAddress;
+            Data = data;
+        }
+        #endregion
         public (string MCC, string MNC, string LAC, string CID) Data { get; protected set; }
         public (double lat, double lon) Location { get; protected set; }
         public string Address { get; protected set; }
         public bool HasLocation => Location.lat > 0;
         public bool HasAddress => !string.IsNullOrWhiteSpace(Address);
 
+        #region privates
         private IAddressRetriver _retriverAddress;
         private IRetriverLocation _retriverLocation;
-       
+        #endregion
+
         public abstract bool Retrive();
         public abstract bool GetAddress();
 
